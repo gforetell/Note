e7fd8e08cab33aac21acc8c8375ef7d295a32d69
120048716@qq.com <easy6798>
2017-07-17 14:48:06 +0800

    添加工程

---
+++ Apprunner/gen/com/example/apprunner/BuildConfig.java
@@ -0,0 +1,6 @@
+/** Automatically generated file. DO NOT MODIFY */
+package com.example.apprunner;
+
+public final class BuildConfig {
+    public final static boolean DEBUG = true;
+}
---
+++ Apprunner/src/com/example/apprunner/MainActivity.java
@@ -0,0 +1,8 @@
+package com.example.apprunner;
+
+import android.app.Activity;
+
+public class MainActivity extends Activity {
+
+   
+}
